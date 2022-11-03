5bc32ad45951164bc40b2dcdb999dddea666521e
Agustin Paul <a.agustinpaul@gmail.com>
2022-11-03 15:20:47 -0300

    Clase 03 completada

--- 1821-manipulando-dom-con-js-proyecto_inicial/script.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -3,9 +3,23 @@ const btn = document.querySelector("[data-form-btn]");
 //Arrow functions o funciones anónimas
 const createTask = (evento) => {
     evento.preventDefault();
-    console.log("Crear tarea");
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = "";
+    const content = 
+    `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+
+    list.appendChild(task);
+
+    console.log(content);
 }
 
 btn.addEventListener("click", createTask);
